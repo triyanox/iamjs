8f249a0234f8d2481e3295e5a4b54f4841058bf4
Mohamed Achaq <hi@achaq.dev>
2023-04-15 20:25:48 +0000

    Change package entry point 📦

--- packages/react/vite.config.js
+++ packages/react/vite.config.js
@@ -1,29 +1,31 @@
-const { resolve } = require('path');
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-module.exports = {
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'index.tsx'),
-      name: '@iamjs/react',
-      fileName: 'index',
-      formats: ['es']
-    }
-  },
+export default defineConfig({
   plugins: [
+    react(),
     dts({
-      outputDir: 'dist'
-    }),
-    react()
+      insertTypesEntry: true
+    })
   ],
-  rollupOptions: {
-    external: ['react', 'react-dom'],
-    output: {
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: '@iamjs/next',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
       }
     }
   }
-};
+});
